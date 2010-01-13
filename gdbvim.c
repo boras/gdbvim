37cd040592122507286319c30c882b96bdf5ee82
Bora Sahin <borasah@gmail.com>
2010-01-13 13:37:52 +0200

    tab completion support is added. Needs further testing.

--- gdbvim.c
+++ gdbvim.c
@@ -13,6 +13,7 @@
 
 /* Symbolic constants */
 #define IN_BUF_SIZE	256
+#define OUT_BUF_SIZE	256
 #define GDB_BUF_SIZE	1024
 #define PROG_BUF_SIZE	1024
 #define GDB_CMD_SIZE	256
@@ -24,8 +25,20 @@ extern YY_BUFFER_STATE yy_scan_string(const char *yy_str);
 extern void yy_delete_buffer(YY_BUFFER_STATE b);
 extern void yy_flush_buffer(YY_BUFFER_STATE b);
 
+/* static global variable defitions */
+static gdb_state_t gdbstatus = GDB_STATE_CLI;
+static key_type_t prev_key = KEY_OTHER;
+static int gdb_out = GDB_OUT_ANS;
+
+static int gdb_ptym;
+static int readline_ptym, readline_ptys;
+static int prog_ptym, prog_ptys;
+
+static struct termios save_termios;
+static int gdb_cmd_len;
+
 /* Function definitions */
-gdb_mi_cmd_state_t parse_mi_parsetree(void)
+gdb_cmd_mi_state_t parse_mi_parsetree(void)
 {
 	async_record_t *async_rec_ptr;
 	frame_info_t *finfo_ptr;
@@ -41,7 +54,7 @@ gdb_mi_cmd_state_t parse_mi_parsetree(void)
 		logger(str, strlen(str), 0);
 		logger("\n", 1, 0);
 		free(str);
-		return GDB_MI_CMD_COMPLETED;
+		return GDB_CMD_MI_COMPLETED;
 	}
 	else {
 		/* Print console stream messages */
@@ -52,13 +65,13 @@ gdb_mi_cmd_state_t parse_mi_parsetree(void)
 			finfo_ptr = mi_get_frame(async_rec_ptr);
 			mi_print_frame_info(finfo_ptr);
 			free_frame_info(finfo_ptr);
-			return GDB_MI_CMD_COMPLETED;
+			return GDB_CMD_MI_COMPLETED;
 		}
 		else /* We have not got it yet */
-			return GDB_MI_CMD_INCOMPLETED;
+			return GDB_CMD_MI_INCOMPLETED;
 	}
 
-	return GDB_MI_CMD_COMPLETED;
+	return GDB_CMD_MI_COMPLETED;
 }
 
 int create_mi_parsetree(char *str)
@@ -83,23 +96,6 @@ int create_mi_parsetree(char *str)
 	yy_delete_buffer(bufstate);
 
 	return ret;
-}
-
-gdb_mi_cmd_state_t handle_mi_output(char *gdbbuf)
-{
-	gdb_mi_cmd_state_t mi_cmd_status;
-
-	if (!create_mi_parsetree(gdbbuf)) {
-		/* There is a valid parse tree */
-		mi_cmd_status = parse_mi_parsetree();
-		destroy_gdbmi_output();
-		gdbmi_out_ptr = NULL;
-
-		return mi_cmd_status;
-	}
-	/* Parse tree could not be created */
-
-	return GDB_MI_CMD_COMPLETED;
 }
 
 int get_mi_output(int ptym, char *gdbbuf)
@@ -146,9 +142,6 @@ int get_mi_output(int ptym, char *gdbbuf)
 	return nread;
 }
 
-static gdb_state_t gdbstatus = GDB_STATE_CLI;
-static int gdb_ptym;
-
 /* Strip whitespace(s) from the start and end of a str */
 char *stripws(char *str)
 {
@@ -170,6 +163,198 @@ char *stripws(char *str)
 	*++tail = '\0';
 
 	return head;
+}
+
+void turn_echo_off(int fd)
+{
+	struct termios stermios;
+
+	/* Turn echo'ing off */
+	tcgetattr(fd, &stermios);
+	stermios.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL);
+	tcsetattr(fd, TCSANOW, &stermios);
+}
+
+void turn_echo_on(int fd)
+{
+	struct termios stermios;
+
+	/* Turn echo'ing on */
+	tcgetattr(fd, &stermios);
+	stermios.c_lflag |= ECHO | ECHOE | ECHOK | ECHONL;
+	tcsetattr(fd, TCSANOW, &stermios);
+}
+
+/*
+ * There might be four different situation:
+ *	1. "erase line echo" coming from tab completion request.
+ *	2. "erase line + cmd echo" coming from nearly every gdb cmd be it
+ *	cli or mi.
+ *	3. "messages" from gdb without associated gdb cmd. Afaik, the only
+ *	one is gdb version output which shows up at the start of gdb.
+ *	4. "cmd echo" coming from answers to gdb querying cmds. This may
+ *	change in the future.
+ */
+static char *kill_echo(char *char_ptr, int cmd_echo)
+{
+	char *ans_ptr = char_ptr;
+
+	/*
+	 * erase_line brings two responses: \a (bell ring) if there is no
+	 * input or an escape sequence if there are some input. Escape
+	 * sequence has two forms: if the command is short, then some \b
+	 * (backspace) which equals to the length of the cmd and a erase
+	 * line esc sequence; otherwise a carriage return and some movement
+	 * then an erase line escape sequence. Some examples can make a long
+	 * story shorter.
+	 *
+	 * prompt = (gdb) next
+	 * req = erase line
+	 * ans = \b\b\b\b\033[K
+	 *
+	 * prompt = (gdb) file[space]
+	 * req = erase line
+	 * ans = \b\b\b\b\b\033[K
+	 *
+	 * prompt = (gdb) add-file-symbol
+	 * req = erase line
+	 * ans = \r\033[C\033[C\033[C\033[C\033[C\033[C\033[K
+
+	 * \b -> backspace
+	 * \r -> move to beginning of line
+	 * \033[C -> forward a character. Six times equal to (gdb)[space]
+	 * \033[K -> clear from cursor to end of screen in this case
+	 *
+	 * As it seems they all share a common suffix \033[K.
+	 */
+	if (*ans_ptr == '\a')
+		ans_ptr++;
+	else {
+		do {
+			ans_ptr = strchr(ans_ptr, '\033');
+			if (!ans_ptr)
+				break;
+			ans_ptr++;
+			if (!strncmp(ans_ptr, "[K", 2)) {
+				ans_ptr += 2;
+				break;
+			}
+			ans_ptr += 2;
+		} while (*ans_ptr);
+	}
+
+	/* Buffer does not contain any erase line output */
+	if (!ans_ptr || !*ans_ptr)
+		ans_ptr = char_ptr;
+
+	if (cmd_echo && gdb_out == GDB_OUT_ECHO)
+		ans_ptr += gdb_cmd_len;
+
+	if (ans_ptr != char_ptr)
+		return ans_ptr;
+	return char_ptr;
+}
+
+static void erase_line(int fd)
+{
+	char c = 0x15;
+
+	write(fd, &c, 1);
+}
+
+gdb_cmd_mi_state_t handle_mi_output(char *gdbbuf)
+{
+	gdb_cmd_mi_state_t mi_cmd_status;
+	char *ans_ptr;
+
+	if (gdb_out == GDB_OUT_ECHO) {
+		/* This means echo'ing will be cut */
+		ans_ptr = kill_echo(gdbbuf, 1);
+		gdb_out = GDB_OUT_ANS;
+	}
+
+	if (!create_mi_parsetree(ans_ptr)) {
+		/* There is a valid parse tree */
+		mi_cmd_status = parse_mi_parsetree();
+		destroy_gdbmi_output();
+		gdbmi_out_ptr = NULL;
+
+		return mi_cmd_status;
+	}
+	/* Parse tree could not be created */
+
+	return GDB_CMD_MI_COMPLETED;
+}
+
+void handle_cli_output(char *gdbbuf)
+{
+	char *ans_ptr;
+	int nread;
+
+	nread = read(gdb_ptym, gdbbuf, GDB_BUF_SIZE);
+	gdbbuf[nread] = '\0';
+	ans_ptr = kill_echo(gdbbuf, 1);
+	if (ans_ptr != (gdbbuf + nread)) {
+		/* Comes here after echo'ing is cut */
+		nread -= ans_ptr - gdbbuf;
+		write(STDOUT_FILENO, ans_ptr, nread);
+	}
+	else
+		gdb_out = GDB_OUT_ANS;
+}
+
+int tab_completion(int count, int key)
+{
+	char gdb_cmd_buf[GDB_CMD_SIZE];
+	int nread;
+
+	if (prev_key == KEY_TAB)
+		write(gdb_ptym, "\t", 1);
+	else {
+		erase_line(gdb_ptym);
+		sprintf(gdb_cmd_buf, "%s\t", rl_line_buffer);
+		write(gdb_ptym, gdb_cmd_buf, strlen(gdb_cmd_buf));
+	}
+	gdbstatus = GDB_STATE_CLI_COMPLETION;
+	prev_key = KEY_TAB;
+
+	return 0;
+}
+
+void handle_complete_by_tab_output(char *gdbbuf)
+{
+	char *char_ptr, *ans_ptr;
+	int nread;
+
+	nread = read(gdb_ptym, gdbbuf, GDB_BUF_SIZE);
+	gdbbuf[nread] = '\0';
+	ans_ptr = kill_echo(gdbbuf, 0);
+	if (ans_ptr == (gdbbuf + nread)) {
+		/* Answer does not contain echo */
+		ans_ptr = gdbbuf;
+	}
+
+	if (*ans_ptr == '\a')
+		write(STDOUT_FILENO, "\a", 1);
+	else if ((char_ptr = strchr(ans_ptr, '\a')) != NULL) {
+		if (!*++char_ptr) /* \a is at the end. no change */
+			write(STDOUT_FILENO, "\a", 1);
+		else {
+			rl_insert_text(char_ptr);
+			rl_redisplay();
+			write(STDOUT_FILENO, "\a", 1);
+		}
+	}
+	else if (*ans_ptr != '\n') { /* completion is successful */
+			rl_delete_text(0, rl_end);
+			rl_point = 0;
+			rl_insert_text(ans_ptr);
+			rl_redisplay();
+	}
+	else
+		write(STDOUT_FILENO, ans_ptr, strlen(ans_ptr));
+
+	gdbstatus = GDB_STATE_CLI;
 }
 
 /* Called when EOF or newline is encountered */
@@ -196,28 +381,37 @@ void handle_gdb_input(char *line)
 		 * it strips newline. However, gdb commands should be ended
 		 * with a newline so we set it again.
 		 */
+		// FIXME: Why we need this?
 		if (!*line || !*cmd) { /* previous command */
 			/* readline gives: line = "" */
 			if (prev_cmd_type == GDB_CMD_MI)
 				gdbstatus = GDB_STATE_MI;
 			else
 				gdbstatus = GDB_STATE_CLI;
+			erase_line(gdb_ptym);
+			/* newline produces n\n as echo */
+			gdb_cmd_len = 1;
 			write(gdb_ptym, "\n", 1);
 		}
 		else if (*cmd == '-') { /* gdb/mi command */
 			/* readline gives: -exec-next'\0' */
 			prev_cmd_type = GDB_CMD_MI;
 			gdbstatus = GDB_STATE_MI;
+			erase_line(gdb_ptym);
 			sprintf(gdb_cmd_buf, "interpreter mi %s\n", cmd);
-			write(gdb_ptym, gdb_cmd_buf, strlen(gdb_cmd_buf));
+			gdb_cmd_len = strlen(gdb_cmd_buf);
+			write(gdb_ptym, gdb_cmd_buf, gdb_cmd_len);
 		}
 		else { /* gdb/cli command */
 			/* readline gives: line = next'\0' */
 			prev_cmd_type = GDB_CMD_CLI;
 			gdbstatus = GDB_STATE_CLI;
+			erase_line(gdb_ptym);
 			sprintf(gdb_cmd_buf, "%s\n", cmd);
-			write(gdb_ptym, gdb_cmd_buf, strlen(gdb_cmd_buf));
+			gdb_cmd_len = strlen(gdb_cmd_buf);
+			write(gdb_ptym, gdb_cmd_buf, gdb_cmd_len);
 		}
+		gdb_out = GDB_OUT_ECHO;
 
 		free(line);
 	}
@@ -226,13 +420,18 @@ void handle_gdb_input(char *line)
 	}
 }
 
-void handle_user_input(gdbvim_t *gv_h, char *inbuf)
+void handle_user_input(char *inbuf)
 {
 	int nread;
 
 	/* gdb or prog input */
-	if (gdbstatus == GDB_STATE_CLI) /* input for gdb */
-		rl_callback_read_char();
+	if (gdbstatus == GDB_STATE_CLI) { /* input for gdb */
+		/*rl_callback_read_char();*/
+		nread = read(STDIN_FILENO, inbuf, IN_BUF_SIZE);
+		if (*inbuf != '\t')
+			prev_key = KEY_OTHER;
+		write(readline_ptym, inbuf, nread);
+	}
 	else { /* input for prog */
 		/*
 		 * The only possibility for a program to get input is the
@@ -242,16 +441,17 @@ void handle_user_input(gdbvim_t *gv_h, char *inbuf)
 		 * in the mi state.
 		 */
 		nread = read(STDIN_FILENO, inbuf, IN_BUF_SIZE);
-		write(gv_h->prog_ptym, inbuf, nread);
+		write(prog_ptym, inbuf, nread);
 	}
 }
 
-int main_loop(gdbvim_t *gv_h)
+int main_loop(void)
 {
 	char inbuf[IN_BUF_SIZE];
+	char outbuf[OUT_BUF_SIZE];
 	char gdbbuf[GDB_BUF_SIZE];
 	char progbuf[PROG_BUF_SIZE];
-	struct pollfd fds[3];
+	struct pollfd fds[5];
 	int nread;
 	int ret;
 
@@ -260,17 +460,23 @@ int main_loop(gdbvim_t *gv_h)
 	fds[0].events = POLLIN;
 	fds[1].fd = gdb_ptym;
 	fds[1].events = POLLIN;
-	fds[2].fd = gv_h->prog_ptym;
+	fds[2].fd = prog_ptym;
 	fds[2].events = POLLIN;
+
+	fds[3].fd = readline_ptys;
+	fds[3].events = POLLIN;
+	fds[4].fd = readline_ptym;
+	fds[4].events = POLLIN;
 
 	/* The main loop */
 	while (1) {
 		/* Wait for indefinetely */
-		if ((ret = poll(fds, 3, -1)) <= 0) {
+		if ((ret = poll(fds, 5, -1)) <= 0) {
 			fprintf(stderr, "Poll error\n");
 			perror(__FUNCTION__);
 			return ret;
 		}
+
 		if (fds[0].revents == POLLIN) {
 			/*
 			 * gdb/cli or prog input. Commands are read by
@@ -278,9 +484,22 @@ int main_loop(gdbvim_t *gv_h)
 			 * Afterwards they are converted to gdb/mi input
 			 * commands.
 			 */
-			handle_user_input(gv_h, inbuf);
+			handle_user_input(inbuf);
 		}
-		if (fds[1].revents == POLLIN) {
+
+		if (fds[3].revents == POLLIN) /* readline input */
+			rl_callback_read_char();
+		if (fds[4].revents == POLLIN) { /* readline output */
+			nread = read(fds[4].fd, outbuf, OUT_BUF_SIZE);
+			write(STDOUT_FILENO, outbuf, nread);
+		}
+
+		if (fds[2].revents == POLLIN) { /* prog output */
+			nread = read(fds[2].fd, progbuf, PROG_BUF_SIZE);
+			write(STDOUT_FILENO, progbuf, nread);
+		}
+
+		if (fds[1].revents == POLLIN) { /* gdb output */
 			/*
 			 * gdb output. Two things should be done.
 			 * 1. gdb/mi output is converted to the gdb/cli form.
@@ -288,24 +507,20 @@ int main_loop(gdbvim_t *gv_h)
 			 * Vim show the file and line in question by using
 			 * signs.
 			 */
-			if (gdbstatus == GDB_STATE_CLI) {
-				nread = read(fds[1].fd, gdbbuf, GDB_BUF_SIZE);
-				write(STDOUT_FILENO, gdbbuf, nread);
-			}
+			if (gdbstatus == GDB_STATE_CLI)
+				handle_cli_output(gdbbuf);
+			else if (gdbstatus == GDB_STATE_CLI_COMPLETION)
+				handle_complete_by_tab_output(gdbbuf);
 			else { /* GDB_MI_STATE */
 				/* Get it as a block */
 				if (!get_mi_output(fds[1].fd, gdbbuf)) {
 					if (handle_mi_output(gdbbuf) ==
-					    GDB_MI_CMD_COMPLETED)
+					    GDB_CMD_MI_COMPLETED)
 						gdbstatus = GDB_STATE_CLI;
 					else
 						gdbstatus = GDB_STATE_MI;
 				}
 			}
-		}
-		if (fds[2].revents == POLLIN) { /* prog output */
-			nread = read(fds[2].fd, progbuf, PROG_BUF_SIZE);
-			write(STDOUT_FILENO, progbuf, nread);
 		}
 	}
 
@@ -381,14 +596,91 @@ static int parse_args(int argc, char *argv[])
 	return 0;
 }
 
-void turn_echo_off(int fd)
+int tty_cbreak(int fd)
 {
-	struct termios stermios;
+	struct termios buf;
 
-	/* Turn echo'ing off */
-	tcgetattr(fd, &stermios);
-	stermios.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL);
-	tcsetattr(fd, TCSANOW, &stermios);
+	if (tcgetattr(fd, &save_termios) < 0)
+		return -1;
+
+	buf = save_termios;
+	buf.c_lflag &= ~(ECHO | ICANON);
+	buf.c_iflag &= ~(ICRNL | INLCR);
+	buf.c_cc[VMIN] = 1;
+	buf.c_cc[VTIME] = 0;
+
+	buf.c_cc[VLNEXT] = _POSIX_VDISABLE;
+
+#if defined(VDSUSP) && defined(_POSIX_VDISABLE)
+	buf.c_cc[VDSUSP] = _POSIX_VDISABLE;
+#endif
+
+	/* disable flow control; let ^S and ^Q through to pty */
+	buf.c_iflag &= ~(IXON|IXOFF);
+#ifdef IXANY
+	buf.c_iflag &= ~IXANY;
+#endif
+
+	if (tcsetattr(fd, TCSAFLUSH, &buf) < 0)
+		return -1;
+
+	return 0;
+}
+
+/* Sets the terminal attributes back to their previous state */
+void tty_reset(int fd)
+{
+	tcsetattr(fd, TCSAFLUSH, &save_termios);
+}
+
+void sigint (int s)
+{
+	tty_reset(STDIN_FILENO);
+	close(readline_ptym);
+	close(readline_ptys);
+	close(gdb_ptym);
+
+	exit(0);
+}
+
+static void custom_deprep_term_function(void) {}
+
+static int init_readline (void)
+{
+	FILE *input, *output;
+	int ret;
+
+	ret = openpty(&readline_ptym, &readline_ptys, NULL, NULL, NULL);
+	if (ret == -1)
+		return -1;
+
+	if (!(input = fdopen(readline_ptys, "r")))
+		return -1;
+
+	if (!(output = fdopen(readline_ptys, "w")))
+		return -1;
+	rl_instream = input;
+	rl_outstream = output;
+
+	/*rl_bind_key('\t', rl_insert);*/
+	rl_bind_key('\t', tab_completion);
+
+	rl_already_prompted = 1;
+	rl_callback_handler_install("(gdb) ", handle_gdb_input);
+
+	/* Set the terminal type to dumb so the output of readline can be
+	* understood by tgdb */
+	/*if (rl_reset_terminal("dumb") == -1)*/
+		/*return -1;*/
+
+	/*
+	 * For some reason, readline can not deprep the terminal.
+	 * However, it doesn't matter because no other application is
+	 * working on the terminal besides readline.
+	 */
+	rl_deprep_term_function = custom_deprep_term_function;
+
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -410,13 +702,17 @@ int main(int argc, char *argv[])
 	}
 
 	/* Initialize readline */
-	rl_bind_key('\t', rl_insert);
-	rl_already_prompted = 1;
-	rl_callback_handler_install("(gdb) ", handle_gdb_input);
+	if (init_readline() < 0)
+		return -1;
+
+	if (tty_cbreak(STDIN_FILENO) < 0)
+		return -1;
+
+	signal(SIGINT, sigint);
 
 	//FIXME: Close an open pseudo terminal
 	/* Pseudo terminal for the program being debugged */
-	ret = openpty(&gv_h->prog_ptym, &gv_h->prog_ptys, NULL, NULL, NULL);
+	ret = openpty(&prog_ptym, &prog_ptys, NULL, NULL, NULL);
 	if (ret < 0) {
 		fprintf(stderr, "No pseudo tty left\n");
 		goto err_out;
@@ -438,7 +734,7 @@ int main(int argc, char *argv[])
 	 * this command is intermixed. gdb command prompt shows
 	 * this annoying output.
 	 */
-	sprintf(gdb_args, "--tty=%s", ptsname(gv_h->prog_ptys));
+	sprintf(gdb_args, "--tty=%s", ptsname(prog_ptys));
 
 	/* Child is created with a pseudo controlling terminal */
 	gv_h->gdb_pid = forkpty(&gdb_ptym, NULL, NULL, NULL);
@@ -449,7 +745,7 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 	else if (gv_h->gdb_pid == 0) {	/* Child */
-		turn_echo_off(STDIN_FILENO);
+		/*turn_echo_off(gdb_ptym);*/
 		/* Turn NL -> CR/NL output mapping off */
 		tcgetattr(STDIN_FILENO, &stermios);
 		stermios.c_oflag &= ~(ONLCR);
@@ -465,8 +761,9 @@ int main(int argc, char *argv[])
 	 *	stdin -> gdb_ptym or prog_ptym,
 	 *	stdout <- gdb_ptym or prog_ptym
 	 */
-	main_loop(gv_h);
+	main_loop();
 
+	tty_reset(STDIN_FILENO);
 err_out:
 	free(gv_h);
 
--- gdbvim.h
+++ gdbvim.h
@@ -3,25 +3,34 @@
 
 #include "parser.h"
 
+typedef enum key_type {
+	KEY_TAB,
+	KEY_OTHER
+} key_type_t;
+
+typedef enum gdb_out_type {
+	GDB_OUT_ECHO,
+	GDB_OUT_ANS
+} gdb_out_type_t;
+
 typedef enum gdb_cmd_type {
 	GDB_CMD_CLI,
 	GDB_CMD_MI
 } gdb_cmd_type_t;
 
-typedef enum gdb_mi_cmd_state {
-	GDB_MI_CMD_COMPLETED,
-	GDB_MI_CMD_INCOMPLETED,
-} gdb_mi_cmd_state_t;
+typedef enum gdb_cmd_mi_state {
+	GDB_CMD_MI_COMPLETED,
+	GDB_CMD_MI_INCOMPLETED,
+} gdb_cmd_mi_state_t;
 
 typedef enum gdb_state {
 	GDB_STATE_CLI,
+	GDB_STATE_CLI_COMPLETION,
 	GDB_STATE_MI
 } gdb_state_t;
 
 typedef struct gdbvim {
 	pid_t gdb_pid;
-	int prog_ptym;	/* master */
-	int prog_ptys;	/* slave */
 } gdbvim_t;
 
 #endif /* __GDBVIM_H__ */
